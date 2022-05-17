d6cd5201a0cb144b43885650c85e6060180e36bb
JimiMacJimi0101 <102225605+JimiMacJimi0101@users.noreply.github.com>
2022-05-17 18:53:38 +0200

    Update views.py

--- food/views.py
+++ food/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'food/index.html', context=None)
+
+def send(request):
+    
+    sprava = request.GET["sprava"]
+    
+    return render(request,'index.html',{'sprava':sprava})
